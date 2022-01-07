ef4916ed586262ef5e48b3728905df187d53fff1
Hyebeen.So <hyebeen0328@naver.com>
2022-01-07 11:34:25 +0900

    1월 1주차 과제 - IPv6

--- algorithm/IPv6_소혜빈.java
+++ algorithm/IPv6_소혜빈.java
@@ -8,6 +8,7 @@ import static java.util.Arrays.asList;
 import static java.util.Arrays.stream;
 import static java.util.Collections.fill;
 import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 
 
 public class IPv6_소혜빈 {
@@ -17,43 +18,51 @@ public class IPv6_소혜빈 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringBuilder ipv6 = new StringBuilder();
         String shortFormIpv6 = br.readLine();
+        List<String> arrIpv6 = new ArrayList<>();
 
-        if(shortFormIpv6.indexOf(doubleColon) < 0) {
-            ipv6.append(decompressIpv6ByRule1(shortFormIpv6.split(colon)));
-        }
-        else {
-            int compressed0000Cnt = 8;
-            String[] splittedIp = shortFormIpv6.split(doubleColon);
+        String[] splittedIp = shortFormIpv6.split(doubleColon);
+        List<String> decompressedFrontIp = getDecompressedFrontIp(splittedIp);
+        List<String> decompressedBackIp = getDecompressedBackIp(splittedIp);
 
-            String[] frontIp = splittedIp[0].split(colon);
-            String decompressedFrontIp = decompressIpv6ByRule1(frontIp);
-            compressed0000Cnt -= frontIp.length;
-            ipv6.append(decompressedFrontIp);
+        int compressed0000Cnt = 8 - (decompressedFrontIp.size() + decompressedBackIp.size());
+        List<String> decompressed0000List = getDecompressed0000List(compressed0000Cnt);
 
-            StringBuilder decompressedBackIp = new StringBuilder();
-            if(splittedIp.length > 1) {
-                String[] backIp = splittedIp[1].split(colon);
-                decompressedBackIp.append(colon).append(decompressIpv6ByRule1(backIp));
-                compressed0000Cnt -= backIp.length;
-            }
-
-            List<String> decompressed0000List = new ArrayList<>(asList(new String[compressed0000Cnt]));
-            fill(decompressed0000List, "0000");
-            StringBuilder middleIp = new StringBuilder(colon);
-            middleIp.append(decompressed0000List.stream().collect(joining(colon)));
-            ipv6.append(middleIp).append(decompressedBackIp);
-        }
+        arrIpv6.addAll(decompressedFrontIp);
+        arrIpv6.addAll(decompressed0000List);
+        arrIpv6.addAll(decompressedBackIp);
+        String ipv6 = arrIpv6.stream().collect(joining(colon)).toString();
 
         System.out.println(ipv6);
 
         br.close();
     }
 
-    private static String decompressIpv6ByRule1(String[] array) {
+    private static List<String> decompressIpv6ByRule1(String[] array) {
         return stream(array)
                 .map(v -> String.format("%4s", v).replace(' ', '0'))
-                .collect(joining(colon));
+                .collect(toList());
+    }
+
+    private static List<String> getDecompressedFrontIp(String[] splittedIp) {
+        String[] frontIp = splittedIp[0].split(colon);
+        List<String> decompressedFrontIp = decompressIpv6ByRule1(frontIp);
+        return decompressedFrontIp;
+    }
+
+    private static List<String> getDecompressedBackIp(String[] splittedIp) {
+        List<String> decompressedBackIp = new ArrayList<>();
+
+        if (splittedIp.length > 1) {
+            String[] backIp = splittedIp[1].split(colon);
+            decompressedBackIp.addAll(decompressIpv6ByRule1(backIp));
+        }
+        return decompressedBackIp;
+    }
+
+    private static List<String> getDecompressed0000List(int compressed0000Cnt) {
+        List<String> decompressed0000List = new ArrayList<>(asList(new String[compressed0000Cnt]));
+        fill(decompressed0000List, "0000");
+        return decompressed0000List;
     }
 }
