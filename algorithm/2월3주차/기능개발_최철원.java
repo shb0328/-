85f3ee514f8c0ee60c499b18820dc3f01f1ce65c
cheolwon1994 <32122076+cheolwon1994@users.noreply.github.com>
2022-02-16 18:50:06 +0900

    Create 기능개발_최철원.java

---
+++ algorithm/2월3주차/기능개발_최철원.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        int num = progresses.length;
+        for(int i=0;i<num;i++){
+            progresses[i] = (int)Math.ceil((double)(100-progresses[i])/speeds[i]);
+            int val = progresses[i];
+        }
+        
+        int idx=0,cnt=0,day=0;
+        List<Integer> li = new ArrayList<>();
+        while(idx<num){
+            if(day<progresses[idx]){
+                if(cnt>0)   li.add(cnt);
+                day=progresses[idx];
+                cnt=1;
+                idx++;
+            }
+            else{
+                while(idx<num && day>=progresses[idx]){
+                    idx++;
+                    cnt++;
+                }
+            }
+            if(idx==num) li.add(cnt);
+        }
+        
+        int len = li.size();
+        int[] answer = new int[len];
+        for(int i=0;i<len;i++)
+            answer[i]=li.get(i);
+        
+        return answer;
+    }
+}
