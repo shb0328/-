7ee612c51ae9117c35ba9e571ac1394da9342e4b
cheolwon1994 <32122076+cheolwon1994@users.noreply.github.com>
2022-03-07 22:24:43 +0900

    Update [WA]110 옮기기_최철원.cpp

--- algorithm/3월2주차/[WA]110 옮기기_최철원.cpp
+++ algorithm/3월2주차/[WA]110 옮기기_최철원.cpp
@@ -15,7 +15,7 @@ vector<string> solution(vector<string> s) {
             dq.push_back(c);
             idx++;
             if(idx>2){
-                if(dq[idx-3]=='1' && dq[idx-2]=='1' && dq[idx-1]=='0'){
+                if(dq[idx-3]=='1' && dq[idx-2]=='1' && dq[idx-1]=='0'){     //110
                     dq.pop_back();
                     dq.pop_back();
                     dq.pop_back();
@@ -24,38 +24,22 @@ vector<string> solution(vector<string> s) {
                 }
             }
         }
-        string result="";
-        if(idx<3){      //1, 01, 11을 잡기 위해
-            bool addFirst = false;
-            if(idx==1 && dq[0]=='1') addFirst=true;
-            else if(idx==2 && dq[1]=='1') addFirst=true;
-            
-            for(int i=0;i<idx;i++)
-                result+=dq[i];
-            
-            if(addFirst){
-                while(cnt-->0)
-                    result="110"+result;
-            }
-            else{
-                while(cnt-->0)
-                    result+="110";
-            }
-        }
-        else{
-            for(int i=0;i<idx;i++){
-                if(i<idx-2 && dq[i]=='1' && dq[i+1]=='1' && dq[i+2]=='1'){
-                    while(cnt--)
-                        result+="110";
-                    while(i<idx)
-                        result+=dq[i++];
+        string result="",startIdx=-1;
+        for(int i=0;i<idx-1;i++){
+            startIdx=i;
+            if(dq[i]=='1' && dq[i+1]=='1'){
+                if(i<idx-2){
+                    if(dq[i+2]=='1'){   //111
+                        while(cnt--)
+                            result+="110";
+                        break;
+                    }
                 }
-                else result+=dq[i];
             }
-            while(cnt-->0)
-                result+="110";
-            
+            else result+=dq[i];
         }
+        while(cnt--)
+            result+="110";
         answer.push_back(result);
     }
     return answer;
