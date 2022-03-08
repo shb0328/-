435a792c220793b714f6a48e31fa3675bdc3a65f
cheolwon1994 <32122076+cheolwon1994@users.noreply.github.com>
2022-03-08 16:57:08 +0900

    Create [WA] Matrix Layer Rotation_최철원.java

---
+++ algorithm/3월2주차/[WA] Matrix Layer Rotation_최철원.java
@@ -0,0 +1,49 @@
+class Result {
+
+    /*
+     * Complete the 'matrixRotation' function below.
+     *
+     * The function accepts following parameters:
+     *  1. 2D_INTEGER_ARRAY matrix
+     *  2. INTEGER r
+     */
+
+    static int row,col;
+    static int arr[][];
+    
+    public static void rotate(int r, int c, int rot,int times){
+        for(int t=0;t<times;t++){
+            int cnt = rot%((r-2*t)*(c-2*t));
+            while(cnt>0){
+                int val = arr[t][t];
+                for(int j=t;j<col-1-t;j++)  arr[t][j] = arr[t][j+1];
+                for(int i=t;i<row-t-1;i++)  arr[i][col-1-t]=arr[i+1][col-1-t];
+                for(int j=col-1-t;j>t;j--)  arr[row-t-1][j]=arr[row-t-1][j-1];
+                for(int i=row-1-t;i>1;i--)  arr[i][t] = arr[i-1][t]; 
+                arr[t+1][t]=val;
+                cnt--;
+            }
+        }
+    }
+    
+    public static void matrixRotation(List<List<Integer>> matrix, int r) {
+    // Write your code here
+        row = matrix.size();
+        col = matrix.get(0).size();
+        arr = new int[row][col];
+        for(int i=0;i<row;i++)
+            for(int j=0;j<col;j++)
+                arr[i][j] = matrix.get(i).get(j);
+        
+        int times = Math.min(row,col)/2;
+        
+        rotate(row,col,r,times);
+        
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++)
+                System.out.print(arr[i][j]+" ");
+            System.out.println();
+        }
+    }
+
+}
