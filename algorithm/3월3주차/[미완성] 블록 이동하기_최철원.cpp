f9b3e38c47e560b23c64940944b08600d99d59ad
cheolwon1994 <32122076+cheolwon1994@users.noreply.github.com>
2022-03-18 11:19:43 +0900

    Update [미완성] 블록 이동하기_최철원.cpp

--- algorithm/3월3주차/[미완성] 블록 이동하기_최철원.cpp
+++ algorithm/3월3주차/[미완성] 블록 이동하기_최철원.cpp
@@ -19,7 +19,7 @@ struct cmp {
 
 map<long long, int> m;
 int arr[100][100];
-int row, col, sd;
+int row, col;
 int dx[8] = { 0,1,1,1,0,-1,-1,-1 };
 int dy[8] = { -1,-1,0,1,1,1,0,-1 };
 int canGo[8];
@@ -65,7 +65,7 @@ bool checkInMap(int y1, int x1, int y2, int x2, int val) {
 void calRotate(int y, int x, int ty, int tx) {
 	for (int i = 0; i < 8; i++)
 		canGo[i] = 10;
-	int ny, nx, cd=0, cnt = 0;
+	int ny, nx, cd=0, cnt = 0,sd;
 	for (int i = 0; i < 8; i += 2) {
 		ny = y + dy[i];
 		nx = x + dx[i];
@@ -113,90 +113,29 @@ void calRotate(int y, int x, int ty, int tx) {
 	}
 }
 
-void moveDirect(int y, int x, int val) {
-	int ay = y;
-	int ax = x;
-	int ny = ay + dy[sd];
-	int nx = ax + dx[sd];
-	int cnt = 1;
-	while (1) {
-		if (canMove(ny, nx)) {     //이동은 가능
-			//이미 존재하는지 확인
-			int nv = val + cnt;
-			if (checkInMap(ay, ax, ny, nx, nv)) {
-				q.push({ ay,ax,nv });
-				q.push({ ny,nx,nv });
-				ay = ny;
-				ax = nx;
-				ny += dy[sd];
-				nx += dx[sd];
-				cnt++;
+void move4Dir(int y1, int x1,int y2, int x2, int val) {
+	for (int i = 0; i < 8; i += 2) {
+		int nay = y1 + dy[i];
+		int nax = x1 + dx[i];
+		int nby = y2 + dy[i];
+		int nbx = x2 + dx[i];
+		int cnt = 1;
+		while (1) {
+			if (canMove(nay, nax) && canMove(nby, nbx)) {     //이동은 가능
+				//이미 존재하는지 확인
+				int nv = val + cnt;
+				if (checkInMap(nay, nax, nby, nbx, nv)) {
+					q.push({ nay,nax,nv });
+					q.push({ nby,nbx,nv });
+					nay += dy[i];
+					nax += dx[i];
+					nby += dy[i];
+					nbx += dx[i];
+					cnt++;
+				}
+				else break;
 			}
 			else break;
 		}
-		else break;
 	}
-}
-
-int solution(vector<vector<int>> board) {
-	int answer = 0;
-	int ny, nx, nv;
-	row = board.size();
-	col = board[0].size();
-	for (int i = 0; i < row; i++)
-		for (int j = 0; j < col; j++)
-			arr[i][j] = board[i][j];
-	q.push({ 0,0,0 });
-	q.push({ 0,1,0 });
-
-	checkInMap(0, 0, 0, 1, 0);
-
-	while (!q.empty()) {
-		int fx = q.front().x;
-		int fy = q.front().y;
-		int cv = q.front().val;
-		q.pop();
-		int sx = q.front().x;
-		int sy = q.front().y;
-		q.pop();
-		long long calVal = calValue(fy, fx, sy, sx);
-		cout << "fy,fx,sy,sx,cv,calVal: " << fy << " " << fx << " " << sy << " " << sx << " " << cv << " " << calVal << endl;
-		if (m[calVal] != cv) continue;
-		if ((fx == col - 1 && fy == row - 1) || (sx == col - 1 && sy == row - 1)) {
-			answer = cv;
-			break;
-		}
-		//fy,fx 기준
-		calRotate(fy, fx, sy, sx);
-		for (int i = 0; i < 8; i += 2) {
-			if (canGo[i] != 10) {
-				ny = fy + dy[i];
-				nx = fx + dx[i];
-				nv = cv + canGo[i];
-				if (checkInMap(fy, fx, ny, nx, nv)) {
-					q.push({ fy,fx,nv });
-					q.push({ ny,nx,nv });
-				}
-			}
-		}
-		//sd 방향으로 전진
-		moveDirect(sy, sx, cv);
-
-		//sy,sx 기준
-		calRotate(sy, sx, fy, fx);
-		for (int i = 0; i < 8; i += 2) {
-			if (canGo[i] != 10) {
-				ny = sy + dy[i];
-				nx = sx + dx[i];
-				nv = cv + canGo[i];
-				if (checkInMap(sy, sx, ny, nx, nv)) {
-					q.push({ sy,sx,nv });
-					q.push({ ny,nx,nv });
-				}
-			}
-		}
-		//sd 방향으로 전진
-		moveDirect(sy, sx, cv);
-	}
-	return answer;
 }
