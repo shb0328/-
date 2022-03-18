28a62b2829123965b528bba3b02a117e0791283a
cheolwon1994 <32122076+cheolwon1994@users.noreply.github.com>
2022-03-18 14:02:40 +0900

    Update [미완성]블록 이동하기_최철원.java

--- algorithm/3월3주차/[미완성]블록 이동하기_최철원.java
+++ algorithm/3월3주차/[미완성]블록 이동하기_최철원.java
@@ -1,3 +1,4 @@
+//3,6번 TC 오답
 import java.util.*;
 import java.io.*;
 
@@ -18,40 +19,58 @@ class Solution {
         }
 	};
 
+    static class Info2 implements Comparable<Info2>{
+        int y,x;
+        public Info2(int y, int x){
+            this.x=x;
+            this.y=y;
+        }
+        @Override
+        public int compareTo(Info2 o){
+            if(this.y==o.y) return Integer.compare(this.x,o.x);
+            return Integer.compare(this.y,o.y);
+        }
+    };
+    
 	final static int dx[] = { 0, 1, 1, 1, 0, -1, -1, -1 };
 	final static int dy[] = { -1, -1, 0, 1, 1, 1, 0, -1 };
 	final static int MAX = 101;
+    static int arr[][];
 	static HashMap<Long,Integer> map;
 	static int bigX, smallX, bigY, smallY, row, col;
 	static int canRot[];
-
-	static boolean cal(int x1, int y1, int x2, int y2, int sumVal) {
-		long first = x1 * MAX + y1;
-		long sec = x2 * MAX + y2;
+    static PriorityQueue<Info> pq;
+    
+    static long getVal(int x1, int y1, int x2, int y2){
+        PriorityQueue<Info2> pq = new PriorityQueue<>();
+        pq.offer(new Info2(y1,x1));
+        pq.offer(new Info2(y2,x2));
+        long first,sec;
+        Info2 ii = pq.poll();
+        first = ii.x*MAX+ii.y;
+        ii = pq.poll();
+        sec = ii.x*MAX+ii.y;
 		long val = first * MAX * MAX + sec;
-		long val2 = sec * MAX * MAX + first;
-		System.out.println("Cal val, val2: "+val+" "+val2);
-		System.out.println("y1,x1,y2,x2,sumVal: "+y1+" "+x1+" "+y2+" "+x2+" "+sumVal);
-		System.out.println("val이 map에 있는가? :"+map.containsKey(val));
-		System.out.println("val2가 map에 있는가? :"+map.containsKey(val2));
-		System.out.println();
-		if(map.containsKey(val) || map.containsKey(val2)) {
-			int originVal = map.get(val);
+        return val;
+    }
+    
+	static boolean cal(int y1, int x1, int y2, int x2, int sumVal) {
+        long val = getVal(x1,y1,x2,y2);
+		if (map.containsKey(val)){
+            int originVal = map.get(val);
 			if(originVal>sumVal) {
 				map.put(val, sumVal);
-				map.put(val2, sumVal);
 				return true;
 			}
 			else return false;
 		}
 		else {
 			map.put(val, sumVal);
-			map.put(val2, sumVal);
 			return true;
 		}
 	}
-
-	static void calRot(int y, int x, int dir,int[][] board) {
+	
+    static void calRot(int y, int x, int dir) {
 		canRot = new int[8];
         for(int i=0;i<8;i++)
             canRot[i]=10;
@@ -64,11 +83,11 @@ class Solution {
 			nd = (nd+1)%8;
 			ny = y+ dy[nd];
 			nx = x+ dx[nd];
-			if(nx>=0 && ny>=0 && nx<col && ny<row && board[ny][nx]==0) {
+			if(checkCanGo(ny,nx)) {
 				nd = (nd+1)%8;
 				ny = y+ dy[nd];
 				nx = x+ dx[nd];
-				if(nx>=0 && ny>=0 && nx<col && ny<row && board[ny][nx]==0) {
+				if(checkCanGo(ny,nx)) {
 					canRot[nd]=Math.min(canRot[nd],val);
 				}
 				else break;
@@ -77,8 +96,6 @@ class Solution {
 		}
 		cnt=3;
 		nd=dir;
-		ny=y;
-		nx=x;
         val=0;
 		//반시계
 		while(cnt-->0) {
@@ -86,11 +103,11 @@ class Solution {
 			nd = (nd+7)%8;
 			ny = y+ dy[nd];
 			nx = x+ dx[nd];
-			if(nx>=0 && ny>=0 && nx<col && ny<row && board[ny][nx]==0) {
+			if(checkCanGo(ny,nx)) {
 				nd = (nd+7)%8;
 				ny = y+ dy[nd];
 				nx = x+ dx[nd];
-				if(nx>=0 && ny>=0 && nx<col && ny<row && board[ny][nx]==0) {
+				if(checkCanGo(ny,nx)) {
 					canRot[nd]=Math.min(canRot[nd],val);
 				}
 				else break;
@@ -100,17 +117,48 @@ class Solution {
 		
 	}
 
-	static boolean checkCanGo(int y, int x, int[][] board) {
-		if(x >= 0 && y >= 0 && x < col && y < row && board[y][x] == 0) return true;
+	static boolean checkCanGo(int y, int x) {
+		if(x >= 0 && y >= 0 && x < col && y < row && arr[y][x] == 0) return true;
 		return false;
 	}
 	
+    static void move4Dir(int y1, int x1, int y2, int x2, int val, int dir){
+        for (int i = 0; i < 8; i += 2) {
+            int nay = y1 + dy[i];
+            int nax = x1 + dx[i];
+            int nby = y2 + dy[i];
+            int nbx = x2 + dx[i];
+            int cnt = 1;
+            while (true) {
+                if (checkCanGo(nay, nax) && checkCanGo(nby, nbx)) {     //이동은 가능
+                    //이미 존재하는지 확인
+                    int nv = val + cnt;
+                    if (cal(nay, nax, nby, nbx, nv)) {
+                        pq.offer(new Info(nay,nax,dir,val+cnt));
+                        nay += dy[i];
+                        nax += dx[i];
+                        nby += dy[i];
+                        nbx += dx[i];
+                        cnt++;
+                    }
+                    else break;
+                }
+                else break;
+            }
+        }
+    }
+    
 	public static int solution(int[][] board) {
 		int answer = 0;
 		row = board.length;
 		col = board[0].length;
+        //초기화
+        arr = new int[row][col];
+        for(int i=0;i<row;i++)
+            for(int j=0;j<col;j++)
+                arr[i][j]=board[i][j];
 		map = new HashMap<>();
-		PriorityQueue<Info> pq = new PriorityQueue<>();
+		pq = new PriorityQueue<>();
 		pq.offer(new Info(0, 0, 2, 0));
 		cal(0, 0, 1, 0, 0);
 
@@ -122,35 +170,35 @@ class Solution {
 			int cd = ii.dir;
 			int sx = ii.x + dx[cd];
 			int sy = ii.y + dy[cd];
-            System.out.println("cy,cx,sy,sx,cv: " + cy + " " + cx + " " + sy + " " + sx + " " + cv);
+            //System.out.println("cy,cx,sy,sx,cv: " + cy + " " + cx + " " + sy + " " + sx + " " + cv);
 			if ((cx == col - 1 && cy == row - 1) || (sx == col - 1 && sy == row - 1)) {
 				answer = cv;
 				break;
 			}
-			calRot(cy, cx, cd, board);
+			calRot(cy, cx, cd);
 			for (int i = 0; i < 8; i += 2) {
 				if (i == cd || canRot[i]==10)	continue;
 				int nsx = cx + dx[i];
 				int nsy = cy + dy[i];
-				if (checkCanGo(nsy,nsx,board)) {
-					if (cal(nsx, nsy, cx, cy,cv+canRot[i]))
+				if (checkCanGo(nsy,nsx)) {
+					if (cal(nsy, nsx, cy, cx,cv+canRot[i]))
 						pq.offer(new Info(cy, cx, i, cv + canRot[i]));
 				}
 			}
-			calRot(sy, sx, (cd+4)%8, board);
+			calRot(sy, sx, (cd+4)%8);
 			for (int i = 0; i < 8; i += 2) {
 				int nd = (i + 4) % 8;
 				if (nd == cd || canRot[i]==10) continue;
 				int nx = sx + dx[i];
 				int ny = sy + dy[i];
-				if (checkCanGo(ny,nx,board)) {
-					if (cal(nx, ny, sx, sy,cv+canRot[i]))
+				if (checkCanGo(ny,nx)) {
+					if (cal(ny, nx, sy, sx,cv+canRot[i]))
 						pq.offer(new Info(sy, sx, i, cv + canRot[i]));
 				}
 			}
 			
 			//그대로 이동
-			for(int i=0;i<8;i+=2) {
+			/*for(int i=0;i<8;i+=2) {
                 if(i%4!=cd%4) continue;
 				int nx = cx+dx[i];
 				int ny = cy+dy[i];
@@ -159,8 +207,8 @@ class Solution {
 				if(checkCanGo(ny,nx,board) && checkCanGo(nsy,nsx,board))
 					if(cal(nx,ny,nsx,nsy,cv+1))
 						pq.offer(new Info(ny,nx,i,cv+1));
-			}
-			
+			}*/
+			move4Dir(cy,cx,sy,sx,cv,cd);
 		}
 		return answer;
 	}
