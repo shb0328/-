e040180e66290580a802f6bd0cd02826335ed676
chaemins193 <63277996+chaemins193@users.noreply.github.com>
2022-04-09 15:37:14 +0900

    Create 플로이드_신채민.java

---
+++ algorithm/4월1주차/플로이드_신채민.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int m = Integer.parseInt(br.readLine());
+		int[][] map = new int[n + 1][n + 1];
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				if (i != j && map[i][j] == 0)
+					map[i][j] = 9999999;
+			}
+		}
+		for (int i = 0; i < m; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			map[start][end] = Math.min(Integer.parseInt(st.nextToken()), map[start][end]);
+		}
+		for (int k = 1; k <= n; k++) {
+			for (int i = 1; i <= n; i++) {
+				if (i == k)
+					continue;
+				for (int j = 1; j <= n; j++) {
+					if (i == j || j == k)
+						continue;
+					if (map[i][j] > map[i][k] + map[k][j]) {
+						map[i][j] = map[i][k] + map[k][j];
+					}
+				}
+			}
+		}
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				if (map[i][j] == 9999999)
+					map[i][j] = 0;
+			}
+		}
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				System.out.print(map[i][j] + " ");
+			}
+			System.out.println();
+		}
+		br.close();
+	}
+
+}
